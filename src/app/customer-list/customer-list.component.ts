dd37e0beed81bab0a78a10902df0160c19289fcc
Rishabh Gupta <rishab178@gmail.com>
2018-10-23 00:51:52 +0530

    Update customer-list.component.ts

--- src/app/customer-list/customer-list.component.ts
+++ src/app/customer-list/customer-list.component.ts
@@ -1,41 +1,42 @@
-import { CustomerService } from './../shared/customer.service';
 import { Component, OnInit } from '@angular/core';
 
+import { CustomerService } from '../shared/customer.service';
+
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  selector: 'app-customer-list',
+  templateUrl: './customer-list.component.html',
+  styleUrls: ['./customer-list.component.css']
 })
-export class CustomerComponent implements OnInit {
- constructor(private customerService: CustomerService) { }
-  submitted: boolean;
-  showSuccessMessage: boolean;
-  formControls = this.customerService.form.controls;
+export class CustomerListComponent implements OnInit {
+
+  constructor(public customerService: CustomerService) { }
+  customerArray = [];
+  showDeletedMessage: boolean;
+  searchText: string = "";
 
   ngOnInit() {
+    this.customerService.getCustomers().subscribe(
+      list => {
+        this.customerArray = list.map(item => {
+          return {
+            $key: item.key,
+            ...item.payload.val()
+          };
+        });
+      });
   }
 
-  onSubmit() {
-    this.submitted = true;
-    if (this.customerService.form.valid) {
-      if (this.customerService.form.get('$key').value == null)
-        this.customerService.insertCustomer(this.customerService.form.value);
-      else
-        this.customerService.updateCustomer(this.customerService.form.value);
-      this.showSuccessMessage = true;
-      setTimeout(() => this.showSuccessMessage = false, 3000);
-      this.submitted = false;
-      this.customerService.form.reset();
-      //this is to be done for proper reset operation
-      this.customerService.form.setValue({
-        $key: null,
-        fullName: '',
-        email: '',
-        mobile: '',
-        location: '',
-        BikeName: ''
-      });
+  onDelete($key) {
+    if (confirm('Are you sure to delete this record ?')) {
+   this.customerService.deleteCustomer($key);
+      this.showDeletedMessage = true;
+      setTimeout(() => this.showDeletedMessage = false, 3000);
     }
+  }
+
+
+  filterCondition(customer) {
+    return customer.fullName.toLowerCase().indexOf(this.searchText.toLowerCase()) != -1;
   }
 
 }
